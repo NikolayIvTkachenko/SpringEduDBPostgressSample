dda5c521536fdfe871dae72e2c049cc3c68b9af1
NikolayTkachenko <tkachenni@mail.ru>
2024-04-13 11:56:17 +0300

    Part 02

--- src/main/java/com/tni/edu/sample/DemoApp/DemoAppApplication.java
+++ src/main/java/com/tni/edu/sample/DemoApp/DemoAppApplication.java
@@ -11,3 +11,20 @@ public class DemoAppApplication {
 	}
 
 }
+
+
+//=================
+//ImplicitNamingStrategyиспользуется, когда явно не указывается имя базы данных, имя таблицы, имя столбца в определении сущности. Т.е. когда не используется аннотация @Table или если есть аннотация @Column, но свойство name не задано.
+//
+//PhysicalNamingStrategyстратегия вступае в работу, когда явно задано имя БД, имя таблицы, имя столбца, например @Column(name = "camelCase"). Вот тогда Hibernate уже будет работать со значением параметра name.
+//
+//Задаются свойства в application.properties:
+
+
+//spring.jpa.hibernate.naming.implicit-strategy=org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy
+//spring.jpa.hibernate.naming.physical-strategy=org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy
+
+//==========
+//org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy
+// which has been deprecated since Spring Boot 2.6 and removed in Spring Boot 3 in favor of the CamelCaseToUnderscoresNamingStrategy.
+// See here: https://docs.spring.io/spring-boot/docs/2.7.1/api/index.html?org/springframework/boot/orm/jpa/hibernate/SpringPhysicalNamingStrategy.html
--- src/main/java/com/tni/edu/sample/DemoApp/entities/City.java
+++ src/main/java/com/tni/edu/sample/DemoApp/entities/City.java
@@ -1,6 +1,7 @@
 package com.tni.edu.sample.DemoApp.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,9 +19,16 @@ public class City {
     @Id
     @Column(name = "city_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private Long id;
 
     @Column(name = "city_name")
     private String cityname;
+
+    @Column(name = "citycode", columnDefinition = "character varying(3) not null default 'NA'")
+    private String citycode;
+
+    @Transient
+    private String beanid;
 
 }
